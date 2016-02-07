519aa351b13b8b199dc47afbc08179b324de0584
Jon Cronin <jonjoncronin@gmail.com>
2016-02-06 22:49:09 -0800

    Cleaning up the linkedList library to make it more generic

--- includes/linkedList_pub.h
+++ includes/linkedList_pub.h
@@ -8,15 +8,7 @@
 #ifndef INCLUDES_LINKEDLIST_PUB_H_
 #define INCLUDES_LINKEDLIST_PUB_H_
 
-/* List Node structures and public APIs */
-typedef struct _linkedListNode
-{
-      int data;
-      struct _linkedListNode *nextNode;
-} linkedListNode;
-
-extern int linkedListNodeEquals(linkedListNode *srcNode, linkedListNode *dstNode);
-
+#include "linkedListNode_pub.h"
 /* Linked List structures and public APIs */
 typedef struct _linkedList
 {
@@ -26,17 +18,14 @@ typedef struct _linkedList
       linkedListNode *tailPtr;
 } linkedList;
 
-extern int linkedListEquals(linkedList *srcList, linkedList *dstList);
-extern int linkedListAddVal(linkedList *list, int val);
+extern int linkedListDoesEntryExist(linkedList *list, linkedListNode *node);
 extern int linkedListAddEntryToHead(linkedList *list, linkedListNode *newNode);
 extern int linkedListAddEntryToTail(linkedList *list, linkedListNode *newNode);
-extern int linkedListAddEntryInOrder(linkedList *list,linkedListNode *newNode);
 extern int linkedListRemoveEntry(linkedList *list, linkedListNode *node);
 extern int linkedListGetLength(linkedList *list);
 extern int linkedListGetMaxSize(linkedList *list);
 extern linkedList* linkedListCreateList(int maxSize);
 extern int linkedListDeleteList(linkedList *list);
 
-extern linkedListNode* linkedListFindEntryWithVal(int val);
 
 #endif /* INCLUDES_LINKEDLIST_PUB_H_ */
--- linkedList/linkedList.c
+++ linkedList/linkedList.c
@@ -4,93 +4,14 @@
  *  Created on: Oct 25, 2014
  *      Author: joncronin
  */
-#include "../includes/linkedList_pub.h"
+#include "linkedList_pub.h"
 #include <stdlib.h>
 
-/*
- * \brief  linkedListEquals compares 2 linkedLists to see if they contain entries
- *         with the same data.
- * \pre    srcList and dstList are valid pointers with the same length.
- * \post   srcList and dstList are never modified
- * \return -2 if input params are invalide, 1 if lists are equal, 0 if not equal
- */
-int linkedListEquals(linkedList *srcList, linkedList *dstList)
+int linkedListDoesEntryExist(linkedList *list, linkedListNode *node)
 {
-   int result = -2;
-   int allEntriesFound = 1;
-   linkedListNode *nodePtr1, *nodePtr2;
-
-   /* check to make sure the inputs are valid pointers */
-   if(!srcList || !dstList)
-   {
-      return result;
-   }
-
-   /* check to make sure the lists have something in them */
-   if(srcList->length != dstList->length)
-   {
-      return result;
-   }
-
-   /* get the head of the destination list */
-   nodePtr1 = dstList->headPtr;
-   while(nodePtr1)
-   {
-      int entryFound = 0;
-      /* get the head of the source list */
-      nodePtr2 = srcList->headPtr;
-      /* go through the source list and compare each node with the current
-       * entry from the destination list
-       */
-      while(nodePtr2)
-      {
-         if(0 == linkedListNodeEquals(nodePtr2,nodePtr1))
-         {
-            entryFound = 1;
-            break;
-         }
-         else
-         {
-            nodePtr2 = nodePtr2->nextNode;
-         }
-      }
-      if(entryFound == 1)
-      {
-         /* get the next entry in the destination list */
-         nodePtr1 = nodePtr1->nextNode;
-      }
-      else
-      {
-         allEntriesFound = 0;
-         break;
-      }
-   }
-
-   return allEntriesFound;
+   return -1;
 }
 
-int linkedListAddVal(linkedList *list, int val)
-{
-   linkedListNode *newNode;
-
-   if(!list)
-   {
-      return -1;
-   }
-
-   if(list->length == list->maxSize)
-   {
-      return -1;
-   }
-   newNode = malloc(sizeof(linkedListNode));
-   if(!newNode)
-   {
-      return -1;
-   }
-
-   newNode->data = val;
-   return linkedListAddEntryInOrder(list,newNode);
-}
 int linkedListAddEntryToHead(linkedList *list, linkedListNode *newNode)
 {
    if(!list || !newNode)
@@ -100,7 +21,6 @@ int linkedListAddEntryToHead(linkedList *list, linkedListNode *newNode)
 
    if(list->length == list->maxSize)
    {
-      free(newNode);
       return -1;
    }
 
@@ -127,15 +47,6 @@ int linkedListAddEntryToTail(linkedList *list, linkedListNode *newNode)
    newNode->nextNode = 0;
 
    return list->length;
-}
-
-int linkedListAddEntryInOrder(linkedList *list,linkedListNode *newNode)
-{
-   //FIXME onla throwing everything onto head for time being, order later
-   // Also need to add error checking
-   linkedListAddEntryToHead(list, newNode);  
-
-   return 1;
 }
 
 int linkedListRemoveEntry(linkedList *list, linkedListNode *node)
@@ -181,10 +92,3 @@ int linkedListDeleteList(linkedList *list)
    free(list);
    return 1;
 }
-
-linkedListNode* linkedListFindEntryWithVal(int val)
-{
-   return NULL;
-}
-
-
